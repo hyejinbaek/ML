0936cb0675616f13d51d6104e0d092e5080807eb
hyejinbaek <bhj4208@gmail.com>
2022-09-16 11:55:07 +0000

    add data load

--- dataset_algo_test.py
+++ dataset_algo_test.py
@@ -2,10 +2,20 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
+# get dataset:adult(uci)
 def get_data():
-    with open('adult.data') as f:
-        df = pd.DataFrame(f)
-        print(df)
+    train_data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
+    #test_data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test'
+    
+    df_train_data = pd.read_csv(train_data_url)
+
+    df_data = df_train_data.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship',
+                                    'race','sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country','and-so-on']
+
+    print(df_train_data.info())
+    
+
+    
 
 
 
