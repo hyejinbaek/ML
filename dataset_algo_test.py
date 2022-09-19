0082b474d96ab2907ae81e6287ecb6618365ed64
hyejinbaek <bhj4208@gmail.com>
2022-09-19 12:34:30 +0000

    non missing data xgboost

--- dataset_algo_test.py
+++ dataset_algo_test.py
@@ -2,23 +2,53 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-# get dataset:adult(uci)
-def get_data():
-    train_data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data'
-    #test_data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.test'
+# get dataset:liver-disorders(uci)
+def non_missing_value():
+    data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+    df_data = pd.read_csv(data_url)
+    col_data = df_data.columns = ['id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion ', 'Single Epithelial Cell Size',
+                                  'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+    #print(df_data)
+    df_data['Bare Nuclei'] = df_data['Bare Nuclei'].replace('?',0)
+    df_data['Bare Nuclei'] = df_data['Bare Nuclei'].astype(int)
+    #print(df_data.info())
+   
     
-    df_train_data = pd.read_csv(train_data_url)
-
-    df_data = df_train_data.columns = ['age', 'workclass', 'fnlwgt', 'education', 'education-num', 'marital-status', 'occupation', 'relationship',
-                                    'race','sex', 'capital-gain', 'capital-loss', 'hours-per-week', 'native-country','and-so-on']
-
-    print(df_train_data.info())
+    from sklearn.model_selection import train_test_split
+    from xgboost import XGBClassifier
     
+    train_col = ['id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion ', 'Single Epithelial Cell Size',
+                'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses']
+    X_train, X_test, y_train, y_test = train_test_split(df_data[train_col], df_data['Class'], test_size = 0.25, random_state = 32)
+    #print(X_train)
 
+    model = XGBClassifier(n_estimators=500, learning_rate=0.2, max_depth=4, random_state = 32)
+    print("model === ", model)
+    model.fit(X_train, y_train)
+    print("model fit 완료")
     
+    from sklearn.metrics import accuracy_score
+
+    y_pred = model.predict(X_test)
+    print("y_pred ====", y_pred)
+    print(accuracy_score(y_pred, y_test))
+    
+def missing_value10():
+    # create missing value
+    data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+    df_data = pd.read_csv(data_url)
+    col_data = df_data.columns = ['id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion ', 'Single Epithelial Cell Size',
+                                  'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+    df_data['Bare Nuclei'] = df_data['Bare Nuclei'].replace('?',0)
+    df_data['Bare Nuclei'] = df_data['Bare Nuclei'].astype(int)
+    
+    print(df_data[['id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion ', 'Single Epithelial Cell Size',
+                'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses']].describe())
+    #print(df_data.describe())
 
 
 
 if __name__ == '__main__':
-    get_data()
+    #non_missing_value()
+    missing_value10()
 
