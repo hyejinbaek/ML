054cf57f00021b6622502a799c62beaa5895b59a
hyejinbaek <bhj4208@gmail.com>
2022-11-18 08:24:38 +0000

    modify

--- test.py
+++ test.py
@@ -1,145 +1,75 @@
-# tabnet
-# 'adult' dataset으로 test 진행
-
-from pytorch_tabnet.tab_model import TabNetClassifier
-import torch
-from sklearn.preprocessing import LabelEncoder
-from sklearn.metrics import roc_auc_score
 import pandas as pd
 import numpy as np
-np.random.seed(0)
-import os
-import wget
-from pathlib import Path
-from matplotlib import pyplot as plt
-os.environ["CUDA_VISIBLE_DEVICES"]= "3"
+from sklearn.model_selection import train_test_split
+import warnings
+import torch
+import torch.nn as nn
+from pytorch_tabnet.tab_model import TabNetClassifier
+from pytorch_tabnet.metrics import Metric
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split, cross_validate, StratifiedKFold
 
-url = "https://archive.ics.uci.edu/ml/machine-learning-databases/adult/adult.data"
-dataset_name = 'census-income'
-out = Path(os.getcwd()+'/data/'+dataset_name+'.csv')
-out.parent.mkdir(parents=True, exist_ok=True)
-if out.exists():
-    print("File already exists.")
-else:
-    print("Downloading file...")
-    wget.download(url, out.as_posix())
+data_url = 'https://archive.ics.uci.edu/ml/machine-learning-databases/breast-cancer-wisconsin/breast-cancer-wisconsin.data'
+df_data = pd.read_csv(data_url)
+col_data = df_data.columns = ['id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
+                              'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+
+df_data['Bare Nuclei'] = df_data['Bare Nuclei'].replace('?',0).astype(int)
+df_data['Class'] = df_data['Class'].replace({2:0, 4:1})
+
+l_enc = LabelEncoder()
+
+
+def cross_valid(
+    X: np.array, y: np.array,
     
-# load data and split
-col_name = ['age','workclass','fnlwgt','education','education-num', 'marital-status', 'occupation' ,
-            'relationship','race','sex','capital-gain','capital-loss','hours-pier-week','native-country',
-            '<=50K']
-train = pd.read_csv(out, names = col_name)
-# print(train)
+    model=TabNetClassifier(cat_idxs=[],  # cat_idxs: List[int] = field(default_factory=list)
+                       cat_dims=[],  # cat_dims: List[int] = field(default_factory=list)
+                       cat_emb_dim=10,     # int = 1
+                       optimizer_fn=torch.optim.Adam,
+                       optimizer_params=dict(lr=1e-3),
+                       scheduler_params={"step_size":50,
+                                         "gamma":0.9},
+                       scheduler_fn=torch.optim.lr_scheduler.StepLR,
+                       mask_type='sparsemax' # "sparsemax", entmax
+                      ),
+    cv=StratifiedKFold(n_splits=5, shuffle = True, random_state=42),
+    scoring=['accuracy', 'f1', 'recall', 'precision'],
+    **kwargs,
+):
+    
+    # model.fit(X_features, y_label) # !! cross_validate 에서 train 동작이 있으므로 지워야 하는 코드 !!
+    cv_result = cross_validate(model, X, y, cv=StratifiedKFold(n_splits=5, shuffle = True, random_state=42), scoring=scoring, **kwargs)
+    
+    for score_name in cv_result:
+        if 'test' in score_name:
+            test_score_mean, test_score_std = np.mean(cv_result[score_name]), np.std(cv_result[score_name])
+            print(f'{score_name}: {test_score_mean:.4f} ± {test_score_std:.4f}') # 유효숫자 소수점 아래 4 자리까지 표시
+            
+from typing import Tuple
 
-target = '<=50K'
-if "Set" not in train.columns:
-    train["Set"] = np.random.choice(["train", "valid", "test"], p =[.8, .1, .1], size=(train.shape[0],))
+train = ['id', 'Clump Thickness', 'Uniformity of Cell Size', 'Uniformity of Cell Shape', 'Marginal Adhesion', 'Single Epithelial Cell Size',
+                              'Bare Nuclei', 'Bland Chromatin', 'Normal Nucleoli', 'Mitoses', 'Class']
+                     
+def set_missing_value(df: pd.DataFrame, ratio: float) -> Tuple[np.array, np.array]:
 
-train_indices = train[train.Set=="train"].index
-valid_indices = train[train.Set=="valid"].index
-test_indices = train[train.Set=="test"].index
+    missing_length = int(len(df) * ratio)
 
-# simple preprocessing
-nunique = train.nunique()
-types = train.dtypes
+    print(missing_length)
+    
+    df = df.copy()
+    df.loc[:missing_length-1, train] = np.nan
+    print("=========df=========", df)
+    #df = df.fillna(0)
+    
+    # df_data[train_col]을 array 형태로 변경
+    X = df[train].to_numpy()
+    # df_data['Class'] : 판다스 series type을 array형태로 변경
+    y = df['Class'].to_numpy()
+    
+    # X: features matrix, y: label vector
+    return X, y
 
-categorical_columns = []
-categorical_dims =  {}
-for col in train.columns:
-    if types[col] == 'object' or nunique[col] < 200:
-        print(col, train[col].nunique())
-        l_enc = LabelEncoder()
-        train[col] = train[col].fillna("VV_likely")
-        train[col] = l_enc.fit_transform(train[col].values)
-        categorical_columns.append(col)
-        categorical_dims[col] = len(l_enc.classes_)
-    else:
-        train.fillna(train.loc[train_indices, col].mean(), inplace=True)
-        
-# Define categorical feature for categorical embeddings
-unused_feat = ['Set']
-features = [ col for col in train.columns if col not in unused_feat+[target]] 
-cat_idxs = [ i for i, f in enumerate(features) if f in categorical_columns]
-cat_dims = [ categorical_dims[f] for i, f in enumerate(features) if f in categorical_columns]
-# define your embedding sizes : here just a random choice
-cat_emb_dim = [5, 4, 3, 6, 2, 2, 1, 10]
-
-# check that pipeline accepts strings
-train.loc[train[target]==0, target] = "wealthy"
-train.loc[train[target]==1, target] = "not_wealthy"
-
-# network parameters
-tabnet_params = {"cat_idxs":cat_idxs,
-                 "cat_dims":cat_dims,
-                 "cat_emb_dim":1,
-                 "optimizer_fn":torch.optim.Adam,
-                 "optimizer_params":dict(lr=2e-2),
-                 "scheduler_params":{"step_size":50, # how to use learning rate scheduler
-                                 "gamma":0.9},
-                 "scheduler_fn":torch.optim.lr_scheduler.StepLR,
-                 "mask_type":'entmax' # "sparsemax"
-                 #"gamma" : 1.3 # coefficient for feature reusage in the masks
-                }
-
-clf = TabNetClassifier(**tabnet_params)
-
-# training
-X_train = train[features].values[train_indices]
-y_train = train[target].values[train_indices]
-
-X_valid = train[features].values[valid_indices]
-y_valid = train[target].values[valid_indices]
-
-X_test = train[features].values[test_indices]
-y_test = train[target].values[test_indices]
-
-max_epochs = 100 if not os.getenv("CI", False) else 2
-
-# This illustrates the warm_start=False behaviour
-save_history = []
-for _ in range(2):
-    clf.fit(
-        X_train=X_train, y_train=y_train,
-        eval_set=[(X_train, y_train), (X_valid, y_valid)],
-        eval_name=['train', 'valid'],
-        eval_metric=['auc'],
-        max_epochs=max_epochs , patience=20,
-        batch_size=1024, virtual_batch_size=128,
-        num_workers=0,
-        weights=1,
-        drop_last=False
-    )
-    save_history.append(clf.history["valid_auc"])
-
-    # plot losses
-plt.plot(clf.history['loss'])
-
-# plot auc
-plt.plot(clf.history['train_auc'])
-plt.plot(clf.history['valid_auc'])
-
-# plot learning rates
-plt.plot(clf.history['lr'])
-
-# prediction
-preds = clf.predict_proba(X_test)
-test_auc = roc_auc_score(y_score=preds[:,1], y_true=y_test)
-
-
-preds_valid = clf.predict_proba(X_valid)
-valid_auc = roc_auc_score(y_score=preds_valid[:,1], y_true=y_valid)
-
-print(f"BEST VALID SCORE FOR {dataset_name} : {clf.best_cost}")
-print(f"FINAL TEST SCORE FOR {dataset_name} : {test_auc}")
-
-# global
-feat_importances = pd.Series(clf.feature_importances_, index=features)
-feat_importances.plot(kind='barh')
-
-#local
-explain_matrix, masks = clf.explain(X_test)
-fig, axs = plt.subplots(1, 3, figsize=(20,20))
-
-for i in range(3):
-    axs[i].imshow(masks[i][30:50])
-    axs[i].set_title(f"mask {i}")
+# xgboost classifier 을 이용할 때 결측치가 없을 때 성능 예측
+X, y = set_missing_value(df_data, 0.2)
+print(cross_valid(X, y))
